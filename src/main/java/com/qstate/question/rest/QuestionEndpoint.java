bd93079c243e04188cd550a1deff3fc643a199ab
PaulHofma <paul_hofma@hotmail.com>
2019-09-10 11:23:22 +0200

    post demo

--- src/main/java/com/qstate/question/rest/QuestionEndpoint.java
+++ src/main/java/com/qstate/question/rest/QuestionEndpoint.java
@@ -1,10 +1,11 @@
 package com.qstate.question.rest;
 
+import com.qstate.question.exceptions.QuestionIdUnknownException;
 import com.qstate.question.model.Question;
 import com.qstate.question.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class QuestionEndpoint {
@@ -14,11 +15,26 @@ public class QuestionEndpoint {
 
     @GetMapping("question")
     public Question testEndpoint(){
-//        System.out.println("new question");
-        Question question = new Question();
-        question.setText("default text");
-        question.setTitle("default title");
-        qs.savequestion();
+        Question question = new Question("default title", "default text");
+        qs.savequestion(question);
         return question;
+    }
+
+    @GetMapping(value="/question/{id}")
+    public Question getQuestion(@PathVariable(name="id") long questionid) {
+        try {
+            Question question = qs.getquestion(questionid);
+            return question;
+        } catch(QuestionIdUnknownException e){
+            System.out.println("The requested id does not match a question");
+            return new Question();
+        }
+    }
+
+    @PostMapping
+    public Question makeQuestion(String title, String text){
+        Question q = new Question(title, text);
+        qs.savequestion(q);
+        return q;
     }
 }
