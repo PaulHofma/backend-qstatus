f818e1372f96cab406eecbbe095b15001f160c47
michiel <pauw.michiel@gmail.com>
2019-09-11 15:54:26 +0200

    Post/Get tips werkt

--- src/main/java/com/qstate/question/service/TipService.java
+++ src/main/java/com/qstate/question/service/TipService.java
@@ -1,7 +1,9 @@
 package com.qstate.question.service;
 
+import com.qstate.question.exceptions.TipIdUnknownException;
 import com.qstate.question.model.Tip;
 import com.qstate.question.persistence.TipRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +14,18 @@ public class TipService {
 
   public Iterable<Tip> getTips() {
     return tr.findAll();
+  }
+
+  public Tip saveTip(Tip tip){
+    return tr.save(tip);
+  }
+
+  public Tip getTip(long tipId) throws TipIdUnknownException {
+    Optional<Tip> tip = tr.findById(tipId);
+    if (tip.isPresent()) {
+      return tip.get();
+    } else {
+      throw new TipIdUnknownException();
+    }
   }
 }
